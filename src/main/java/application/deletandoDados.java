a45858a53ec1c9f2bbb7e01e86fa503bc47f6d22
Rayllanderson <rayllanderson@gmail.com>
2020-07-07 22:00:11 -0300

    deletando dados

--- src/main/java/application/deletandoDados.java
+++ src/main/java/application/deletandoDados.java
@@ -1,9 +1,29 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Cliente;
 
 public class deletandoDados {
 
     public static void main(String[] args) {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("treinamento-jpa");
+	EntityManager em = emf.createEntityManager();
+	
+	em.getTransaction().begin();
+	
+	Cliente cliente = em.find(Cliente.class, 3L);
+	
+	em.remove(cliente);
+	
+	em.getTransaction().commit();
+	
+	System.out.println("deletado com sucesso!");
+	
+	em.close();
+	emf.close();
 
 
     }
